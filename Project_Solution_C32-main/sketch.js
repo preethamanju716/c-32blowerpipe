4c348e04e2c447af43703366ca91b853ac39730e
preethamanju716 <73938378+preethamanju716@users.noreply.github.com>
2021-08-30 16:37:38 +0530

    Add files via upload

---
+++ Project_Solution_C32-main/sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ball;
+var blower;
+var blowerMouth;
+var button;
+
+function setup() {
+  var canvas = createCanvas(500, 500);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  ball = new Ball(width / 2 + 80, height / 2 - 80, 80, 80);
+  blower = new Blower(width / 2 - 50, height / 2 + 50, 150, 20);
+  blowerMouth = new BlowerMouth(width / 2 + 70, height / 2 + 20, 100, 90);
+  //add the button
+  
+}
+
+function draw() {
+  background(59);
+  Engine.update(engine);
+
+  //show the blower,ball,blowerPipe
+}
+
+function blow() {
+  //add the function here
+}
